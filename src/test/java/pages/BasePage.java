dbf70181db6b01a99fae5a8dd3fdaf25302a693f
mada1703 <madalinenache91@gmail.com>
2023-02-27 20:40:21 +0200

    Added base structure

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasePage {
+    private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
+    public static WebDriver driver;
+
+    public static void setUp(){
+        LOG.info("Start test");
+        System.setProperty("webdriver.chrome.driver", "C://Webdrivers/chromedriver110.exe");
+        driver = new ChromeDriver();
+        String url = "https://demo.automationtesting.in/Index.html";
+        driver.get(url);
+
+        LOG.info("Open brwoser maximize");
+        driver.manage().window().maximize();
+    }
+
+    public static void tearDown(){
+        LOG.info("Close the browser");
+        driver.quit();
+    }
+}
